3680b90244d9f92e06bae52dedc93a6ce2352d77
hanshenrik <hh.gronsleth@gmail.com>
2015-04-20 17:54:18 +0100

    added id field, toString prints name and points

--- app/src/main/java/com/hanshenrik/gronsleth_hasamishogi/Player.java
+++ app/src/main/java/com/hanshenrik/gronsleth_hasamishogi/Player.java
@@ -6,21 +6,23 @@ import android.media.Image;
 public class Player {
     public final String name;
     public final String description;
+    public final int id;
     public int points;
     public final Image avatar;
 
-    public Player(String name, String description) {
-        this(name, description, null); // TODO: set to default image instead of null
+    public Player(int id, String name, String description, int points) {
+        this(id, name, description, points, null); // TODO: set to default image instead of null
     }
 
-    public Player(String name, String description, Image avatar) {
+    public Player(int id, String name, String description, int points, Image avatar) {
+        this.id = id;
         this.name = name;
         this.description = description;
-        this.points = 0;
+        this.points = points;
         this.avatar = avatar;
     }
 
     public String toString() {
-        return this.name;
+        return this.name + " (" + this.points + ")";
     }
 }
