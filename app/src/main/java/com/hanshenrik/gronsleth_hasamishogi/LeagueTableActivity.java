c00c4ab8c87a4abcc191e9a155078947432ada80
hanshenrik <hh.gronsleth@gmail.com>
2015-04-20 20:07:09 +0100

    show description and avatar on player click

--- app/src/main/java/com/hanshenrik/gronsleth_hasamishogi/LeagueTableActivity.java
+++ app/src/main/java/com/hanshenrik/gronsleth_hasamishogi/LeagueTableActivity.java
@@ -1,6 +1,9 @@
 package com.hanshenrik.gronsleth_hasamishogi;
 
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,8 +12,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 
 
@@ -19,6 +25,8 @@ public class LeagueTableActivity extends ActionBarActivity {
     private ArrayList<Player> players;
     private ArrayAdapter leagueTableAdapter;
     private ListView leagueTableListView;
+    private TextView descriptionTextView;
+    private ImageView avatarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,27 +40,61 @@ public class LeagueTableActivity extends ActionBarActivity {
         int nameIdx = cursor.getColumnIndexOrThrow(PlayersProvider.KEY_PLAYER);
         int descriptionIdx = cursor.getColumnIndexOrThrow(PlayersProvider.KEY_DESCRIPTION);
         int pointsIdx = cursor.getColumnIndexOrThrow(PlayersProvider.KEY_POINTS);
+        int avatarURLIdx = cursor.getColumnIndexOrThrow(PlayersProvider.KEY_IMAGE_URL);
         if (cursor.moveToFirst()) {
             do {
                 int id = cursor.getInt(idIdx);
                 String name = cursor.getString(nameIdx);
                 String description = cursor.getString(descriptionIdx);
                 int points = cursor.getInt(pointsIdx);
-                players.add(new Player(id, name, description, points));
+                String avatarURL = cursor.getString(avatarURLIdx);
+                players.add(new Player(id, name, description, points, avatarURL));
             } while (cursor.moveToNext());
         }
         cursor.close();
 
         leagueTableListView = (ListView) findViewById(R.id.league_table_list);
+        descriptionTextView = (TextView) findViewById(R.id.description_view);
+        avatarView = (ImageView) findViewById(R.id.avatar_view);
+
         leagueTableAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, players);
         leagueTableListView.setAdapter(leagueTableAdapter);
 
         leagueTableListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Log.d("###", position + "");
+                String description = players.get(position).description;
+                String avatarURL = players.get(position).avatarURL;
+                Log.d("###", avatarURL);
+                descriptionTextView.setText(description);
+                new DownloadImageTask(avatarView).execute(avatarURL);
             }
         });
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urlDisplay = urls[0];
+            Bitmap bitmap = null;
+            try {
+                InputStream in = new java.net.URL(urlDisplay).openStream();
+                bitmap = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return bitmap;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
     }
 
 
